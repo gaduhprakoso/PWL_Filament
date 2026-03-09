3907c76fe3bce3b674c2daeb780ddbefe65ff3b2
gaduhprakoso <gaduhprakoso@gmail.com>
2026-03-10 00:04:48 +0700

    J5_Filament

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -7,11 +7,12 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Group as ComponentsGroup;
+use Filament\Schemas\Components\Section as ComponentsSection;
 
 class PostForm
 {
@@ -19,22 +20,56 @@ class PostForm
     {
         return $schema
             ->components([
-                //
-                TextInput::make('title')->required(),
-                TextInput::make('slug')->required(),
-                Select::make('category_id')
-                    ->relationship('Category', 'name')
-                    ->preload()
-                    ->searchable(),
-                ColorPicker::make('color'),
-                MarkdownEditor::make('body'),
-                // RichEditor::make('body'),
-                FileUpload::make('image')
-                    ->disk('public')
-                    ->directory('post'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                DateTimePicker::make('published_at'),
-            ]);
+
+                // 1. Fields Kiri (Mengambil 2/3 bagian dari total 3 kolom)
+                ComponentsSection::make("Post Details")
+                    ->description("Isi detail utama postingan di sini")
+                    ->icon('heroicon-o-document-text') // Tugas 2: Icon Section 1
+                    ->schema([
+
+                        // Tugas 3: Buat tampilan lebih rapi (2 kolom untuk field utama)
+                        ComponentsGroup::make([
+                            TextInput::make("title")
+                                ->required(),
+                            TextInput::make("slug")
+                                ->required(),
+                            Select::make("category_id")
+                                ->relationship("category", "name")
+                                ->preload()
+                                ->searchable(),
+                            ColorPicker::make("color"),
+                        ])->columns(2),
+
+                        MarkdownEditor::make("content")
+                            ->columnSpanFull(),
+                    ])
+                    ->columnSpan(2), // Tugas 1: Layout Kiri (2/3)
+
+                // 2. Meta Kanan (Mengambil 1/3 bagian sisanya)
+                ComponentsGroup::make([
+
+                    // Section Image
+                    ComponentsSection::make("Post Details")
+                        ::make("Media")
+                        ->icon('heroicon-o-photo') // Tugas 2: Icon Section 2
+                        ->schema([
+                            FileUpload::make("image")
+                                ->disk("public")
+                                ->directory("posts"),
+                        ]),
+
+                    // Section Meta Informasi
+                    ComponentsSection::make("Post Details")
+                        ::make("Meta Information")
+                        ->icon('heroicon-o-information-circle') // Tugas 2: Icon Section 3
+                        ->schema([
+                            TagsInput::make("tags"),
+                            Checkbox::make("published"),
+                            DateTimePicker::make("published_at"),
+                        ]),
+
+                ])->columnSpan(1), // Tugas 1: Layout Kanan (1/3)
+
+            ])->columns(3); // Dasar grid 3 kolom untuk pembagian 2/3 dan 1/3
     }
 }
